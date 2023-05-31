1ec37e837d8ca7def1d7123ff85acb68a0838009
GamalielOP <131983046+GamalielOP@users.noreply.github.com>
2023-05-31 08:58:28 -0600

    e4_31.05.c

---
+++ e4_31.05.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int m=99, n;
+	n= ++m;
+    printf("m=%d, n=%d \n",m,n);
+    n= ++m;
+    printf("m=%d, n=%d \n",m,n);
+    printf("m=%d \n",m++);
+    printf("m=%d \n",++m);
+}
