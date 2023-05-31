2ca2f4c1ed0755c97cb5f0a5bc7870e7837c6d03
GamalielOP <131983046+GamalielOP@users.noreply.github.com>
2023-05-31 08:59:56 -0600

    e6_31.05.c

---
+++ e6_31.05.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int edad, MayorEdad, juvenil;
+	scanf("%i",&edad);
+	 if (edad>18){
+	 	printf("Eres MAYOR");
+	 }
+	 else if(edad<15 && edad<=18){
+	 	printf("Eres MENOR");
+	 }
+	 
+
+	return 0;
+}
