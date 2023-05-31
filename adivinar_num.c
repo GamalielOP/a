a6a3a3e64f07a80c7ab7544857fa84e6e3a28074
GamalielOP <131983046+GamalielOP@users.noreply.github.com>
2023-05-31 12:19:30 -0600

    adivinar_num.c

---
+++ adivinar_num.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+	int main()
+	{
+	int a=8,n=0,i;
+	printf("intenta adivinar el numero\n");
+	scanf("%d",&n);
+	if((n!=a)?(printf("incorrecto"));(printf("correcto")))
+	return 0;
+	}
