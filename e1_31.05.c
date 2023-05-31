7b5ce9d2020ef485ad2f6314baecebe2fc99f47b
GamalielOP <131983046+GamalielOP@users.noreply.github.com>
2023-05-31 08:55:47 -0600

    e1_31.05.c

---
+++ e1_31.05.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+ {
+ 	printf("El tamaño de variables de coma flotante es %d", sizeof (float));
+ 	printf(" bytes\n");
+ 	printf("el tamaño de variables de doble precision es %d", sizeof (double));
+ 	printf(" bytes\n");
+	return 0;
+}#include <stdio.h>
+int main()
+ {
+ 	printf("El tamaño de variables de coma flotante es %d", sizeof (float));
+ 	printf(" bytes\n");
+ 	printf("el tamaño de variables de doble precision es %d", sizeof (double));
+ 	printf(" bytes\n");
+	return 0;
+}
